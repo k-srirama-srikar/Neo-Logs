6c5418e34a7e33e8b4b73ab02d0dd2b6fa6dc6a1
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2025-01-15 19:08:28 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">Blogging Platform</Link>
+        <Link to="/">Tech Chronicles</Link>
       </div>
       <nav className="navbar">
         <ul className="nav-links">
